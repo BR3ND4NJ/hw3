29cc20ff071afcfd5ccc829e7c308e3995823a64
Brendan Johnson <biker8642@gmail.com>
2024-03-19 18:05:01 -0400

    test 1

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws IOException{
         ETree bt = new ETree();
-        bt.buildTree();
+        bt.buildTree(); //hi
     }
 }
